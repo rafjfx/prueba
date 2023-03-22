a789ccb29f4042ddddd836cdaaf3ed9826ff5d1f
rafjfx <126478808+rafjfx@users.noreply.github.com>
2023-03-22 16:03:44 +0100

    nuevo programa para abstract

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import javax.xml.crypto.dsig.spec.DigestMethodParameterSpec;
 import java.awt.event.WindowStateListener;
 import java.security.PublicKey;
@@ -5,28 +6,29 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) {
-        CocheElectrico cocheElectrico = new CocheElectrico();
-        cocheElectrico.velocidadMaxima = 14;
-        cocheElectrico.matricula = "ABC 1234";
-        System.out.println(cocheElectrico.compruebaMatricula("ABC 1234"));
+    Coche coche = new Coche();
+    coche.setSonido("BRRR");
+    System.out.println(coche.getSonido());
     }
 }
-class   Vehiculo {
+abstract class    Vehiculo {
     String matricula;
+    String sonido;
     int velocidadMaxima;
-
-    public boolean compruebaMatricula(String matricula) {
-        if (matricula == "ABC 1234") {
-            return true;
-        }
-        return false;
+    public  Vehiculo(){
+    System.out.println("Estoy en vehiculo");
+}
+    public  String getSonido() {
+        return this.getSonido();
     }
-
+    public void setSonido(String sonido) {
+        this.sonido = sonido;
+    }
 
 }
 class Coche extends Vehiculo {
 
 }
-class CocheElectrico extends Vehiculo {
+class Moto extends Vehiculo {
 
 }
