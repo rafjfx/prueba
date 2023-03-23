2a8e9383f50cf15e1aab6ff8a2629f82d7b2b1aa
rafjfx <126478808+rafjfx@users.noreply.github.com>
2023-03-23 12:57:58 +0100

    nuevo programa para interface

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,23 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) {
+    }
     Coche coche = new Coche();
-    coche.SumaNumeros(3.2, 4.5);
+
+}
+
+interface  Vehiculo {
+    void Acelerar(int cuantaVelocidad);
+    void Frenar(int cuantaVelocidad);
+}
+
+class Coche implements Vehiculo {
+    public void Acelerar(int cuantaVelocidad) {
+
+    }
+
+    @Override
+    public void Frenar(int cuantaVelocidad) {
 
     }
 }
-class    Vehiculo {
-        public void imprime() {
-            System.out.println("Estoy en vehiculo");
-        }
-}
-class Coche extends Vehiculo {
-
-    public void imprime() {
-
-        System.out.println("Estoy en cochde");
-    }
-    public int SumaNumeros(int a, int b) {
-        return a + b;
-    }
-    public float SumaNumeros(float a, float b) {
-        return a + b * (float)9.0;
-    }
-    public void SumaNumeros(double a, double b) {
-        System.out.println("El resultado es: " + (a + b));
-     }
-}
-
