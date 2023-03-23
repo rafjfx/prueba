ec8cb81ff459dab88e180597c14f67c17d6d2802
rafjfx <126478808+rafjfx@users.noreply.github.com>
2023-03-23 12:45:37 +0100

    nuevo programa para polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,29 @@ import java.util.concurrent.Callable;
 public class Main {
     public static void main(String[] args) {
     Coche coche = new Coche();
-    coche.imprime();
+    coche.SumaNumeros(3.2, 4.5);
+
     }
 }
-    class    Vehiculo {
+class    Vehiculo {
         public void imprime() {
             System.out.println("Estoy en vehiculo");
         }
-    }
-    class Coche extends Vehiculo {
+}
+class Coche extends Vehiculo {
 
-                 public void imprime() {
+    public void imprime() {
 
-                      System.out.println("Estoy en cochde");
-                  }
+        System.out.println("Estoy en cochde");
     }
+    public int SumaNumeros(int a, int b) {
+        return a + b;
+    }
+    public float SumaNumeros(float a, float b) {
+        return a + b * (float)9.0;
+    }
+    public void SumaNumeros(double a, double b) {
+        System.out.println("El resultado es: " + (a + b));
+     }
+}
 
