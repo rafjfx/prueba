52a7f37462645d2df1fbb475bd02990d956d0d28
rafjfx <126478808+rafjfx@users.noreply.github.com>
2023-03-23 12:02:15 +0100

    nuevo programa para polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,19 @@ import java.util.concurrent.Callable;
 public class Main {
     public static void main(String[] args) {
     Coche coche = new Coche();
+    coche.imprime();
     }
 }
     class    Vehiculo {
-
-        int velocidadMaxima;
-
-        public Vehiculo() {
+        public void imprime() {
             System.out.println("Estoy en vehiculo");
         }
     }
-    class Motor extends Vehiculo {
-                   String tipomotor;
-                   public Motor() {
-                       System.out.println("Estoy en motor");
-                   }
+    class Coche extends Vehiculo {
+
+                 public void imprime() {
+
+                      System.out.println("Estoy en cochde");
+                  }
     }
-   class Coche extends  Motor {
-}
+
