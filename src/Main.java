613d17417bf548f5e1ef259eb7831a556649f809
rafjfx <126478808+rafjfx@users.noreply.github.com>
2023-03-23 11:56:46 +0100

    nuevo programa para abstract con herencia

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,21 @@ import java.util.concurrent.Callable;
 public class Main {
     public static void main(String[] args) {
     Coche coche = new Coche();
-    coche.setSonido("BRRR");
-    System.out.println(coche.getSonido());
     }
 }
-abstract class    Vehiculo { // clase vehiculo tiene dos funciones abstractas
-    String sonido;
-    int velocidadMaxima;
+    class    Vehiculo {
 
-    public Vehiculo() {
-        System.out.println("Estoy en vehiculo");
-    }
+        int velocidadMaxima;
 
-    abstract public String getSonido();
-
-    abstract public void setSonido(String sonido);
-
-}
-class Coche extends Vehiculo { // clase coche hereda de vehiculo
-    public String getSonido() { // implementa las clase abstracta
-        return "Sonido coche " + this.sonido;
+        public Vehiculo() {
+            System.out.println("Estoy en vehiculo");
+        }
     }
-    public void setSonido(String sonido) {
-        this.sonido = sonido;
+    class Motor extends Vehiculo {
+                   String tipomotor;
+                   public Motor() {
+                       System.out.println("Estoy en motor");
+                   }
     }
-}
-class Moto extends Vehiculo { // clase moto hereda de vehiculo
-    public String getSonido() { // implementa las clase abstracta
-                return "Sonido moto " + this.sonido;
-    }
-    public void setSonido(String sonido) {
-        this.sonido = sonido;
-    }
+   class Coche extends  Motor {
 }
